192c13e087bf549403f18f280beb5dac6242bd73
junwooP0 <163957128+junwoop0@users.noreply.github.com>
2026-03-23 17:22:28 +0900

    Add LeetCode solution for 345: Reverse Vowels of a String

--- leetcode/README.md
+++ leetcode/README.md
@@ -10,3 +10,4 @@ LeetCode solutions for improving programming and problem-solving skills in Pytho
 | 1071. Greatest Common Divisor of Strings | String, Math | Easy | 30 min |
 | 1431. Kids With the Greatest Number of Candies | Array | Easy | 5 min |
 | 605. Can Place Flowers | Array, Greedy | Easy | 20 min |
+| 345. Reverse Vowels of a String | String, Two Pointers | Easy | 15 min |
---
+++ leetcode/string/345_reverse_vowels_of_a_string.py
@@ -0,0 +1,24 @@
+# Problem: 345. Reverse Vowels of a String
+# Time: O(n)
+# Space: O(n)
+# URL: https://leetcode.com/problems/reverse-vowels-of-a-string/description/
+
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        order = []
+        s = list(s)
+        for ch in range(len(s)):
+            if s[ch].lower() in 'aeiou':
+                order.append(s[ch])
+        for ch in range(len(s)):
+            if s[ch].lower() in 'aeiou':
+                s[ch] = order.pop()
+        s = ''.join(s)
+        return s
+
+'''
+Notes
+- Rather then checking vowels like s[i].lower() == 'a' or s[i].lower() == 'e' ... we can use if s[i].lower() in 'aeiou'
+- String is immutable, so we need to convert it to a list to swap characters
+- You can also use two pointers to swap characters
+'''
